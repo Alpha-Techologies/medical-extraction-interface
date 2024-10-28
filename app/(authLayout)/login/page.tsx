39e9015812a446dd35f01849821997621e9f4e78
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-10-28 19:15:59 +0300

    fixed login and sign up pages

--- app/(authLayout)/login/page.tsx
+++ app/(authLayout)/login/page.tsx
@@ -1,59 +1,26 @@
 "use client";
 
-// import Next/React related libraries
 import Link from "next/link";
 import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-
-// import third party libraries
 import "react-toastify/dist/ReactToastify.css";
-import { toast } from "react-toastify";
-
-// import redux toolkit libraries
+import { toast, ToastContainer } from "react-toastify";
 import { useDispatch } from "react-redux";
-
-// import custom utils
-import { useAuth } from "@/hooks/useAuth";
 import { LoginValidation } from "@/utils/schema";
-
-// import icons and ui-libraries
 import { Form, Input, Button } from "antd";
+import { useLoginMutation } from "@/redux/features/user";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
 export default function Login({}: Props) {
-  // local states
-  const [haveProfile, setHaveProfile] = useState(false);
   const [loginForm] = Form.useForm();
+  const [login, { isLoading, error, isError }] = useLoginMutation();
 
   // hook variables
   const router = useRouter();
   const dispatch = useDispatch();
-
-  // auth state
-  // const auth = useAuth();
-  // const {
-  //   data: profileData,
-  //   isLoading: waiting,
-  //   error: theError,
-  //   refetch: getStartupProfile,
-  // } = useGetStartupProfileQuery();
-
-  const {
-    loginHandler,
-    auth: { isAuthenticated, isLoading, error },
-  } = useAuth();
-
-  // re-route to home page if authenticated
-  // useEffect(() => {
-  //   if (isAuthenticated) {
-  //     setShowErrorBorder(false);
-  //     router.replace("/");
-  //   } else {
-  //     setShowErrorBorder(true);
-  //   }
-  // }, [isAuthenticated]);
 
   // Login validation with yup
   const yupSync: any = {
@@ -64,36 +31,24 @@ export default function Login({}: Props) {
 
   let response: any;
   const handleSubmit = async (ev: any) => {
-    const values = loginForm.getFieldsValue();
-    if (values.email && values.password) {
-      const credentials = {
-        email: values.email,
-        password: values.password,
-      };
-      const res = await loginHandler(credentials);
+    const credentials = loginForm.getFieldsValue();
 
-      if (res && "data" in res) {
-        if (res?.data?.success && !res.data.isVerified) {
-          router.push("/app");
-        }
-        if (res?.data?.isVerified) {
-          toast.success("Login Successful");
-        }
-      } else if (res && "error" in res) {
-        const { error } = res as any;
-        if (error.status === "FETCH_ERROR") {
-          toast.error("There is network error!");
-        } else {
-          const errorMessage = error.data?.message;
-          toast.error(errorMessage);
-        }
-      } else {
-      }
+    const res = await login(credentials);
+
+    if (res && "access_token" in res) {
+      toast.success("Account Successfully Created");
+    } else if (res && "error" in res) {
+      const { error } = res as any;
+      const errorMessage = error?.data?.message;
+      toast.error(errorMessage);
+    } else {
+      toast.error("All fields are required");
     }
   };
 
   return (
     <div className='h-full w-full flex items-start justify-center font-inter py-8'>
+      <ToastContainer />
       <div className='w-2/3 gap-4'>
         <div className='flex items-start flex-col mb-8'>
           <h1 className='text-4xl justified font-roboto text-black mb-2'>
@@ -145,9 +100,7 @@ export default function Login({}: Props) {
             </Button>
           </Form.Item>
         </Form>
-        {/* End of Login Form */}
-
-        {/* Forgot Password Link */}
+        {/*   
         <div className='text-center pt-2 mt-3'>
           <div className='text-12 flex justify-center items-center'>
             <Link
@@ -156,7 +109,7 @@ export default function Login({}: Props) {
               Forgot Password?{" "}
             </Link>
           </div>
-        </div>
+        </div> */}
         <div className='text-center pt-2 mt-3'></div>
         {/* End of Forgot Password Link */}
 
@@ -165,18 +118,13 @@ export default function Login({}: Props) {
           <div className='text-12 flex justify-center items-center'>
             <p className='text-black'>I don{"'"}t have an account?</p>
             <Link
-              href='/auth/waitlist'
+              href='/signup'
               className='pl-2'>
               <span className='text-primary hover:underline'>Sign Up</span>
             </Link>
           </div>
         </div>
       </div>
-      {/* End of Signup Link */}
-
-      {/* Toast Container */}
-      {/* <ToastContainer /> */}
-      {/* End of Toast Container */}
     </div>
   );
 }
--- app/(authLayout)/signup/page.tsx
+++ app/(authLayout)/signup/page.tsx
@@ -1,22 +1,19 @@
 "use client";
 
 import Link from "next/link";
-import { toast } from "react-toastify";
 import { Form, Input, Button } from "antd";
-import { selectAuth } from "@/redux/slices/authSlice";
-import { useSelector } from "react-redux";
 import { SignUpValidation } from "@/utils/schema";
 import { useSignupMutation } from "@/redux/features/user";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/navigation";
 
 export default function SignUp() {
   const [signupForm] = Form.useForm();
-  console.log("signup page");
-
-  // auth state
-  const authState = useSelector(selectAuth);
+  const router = useRouter();
   const [signup, { isLoading, error, isError }] = useSignupMutation();
+  // console.log(signup);
 
-  // Signup validation with yup
   const yupSync: any = {
     async validator({ field }: { field: string }, value: string) {
       await SignUpValidation.validateSyncAt(field, { [field]: value });
@@ -26,135 +23,107 @@ export default function SignUp() {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     const credentials = signupForm.getFieldsValue();
     const res = await signup(credentials);
-    console.log(res.error);
+    console.log(res);
     if (res && "access_token" in res) {
       toast.success("Account Successfully Created");
+      router.push("/patients");
     } else if (res && "error" in res) {
       const { error } = res as any;
-      const errorMessage = error.data?.message;
+      const errorMessage = error?.data?.message;
       toast.error(errorMessage);
-    } else {
-      toast.error("All fields are required");
     }
+  };
 
-    return (
-      //@ts-ignore
-      <div className='h-screen w-full flex items-start justify-center font-inter py-8'>
-        <div className='w-2/3 gap-4'>
-          <div className='flex items-start-start flex-col mb-8'>
-            <h1 className='text-4xl justified font-roboto text-black mb-2'>
-              Welcome
-            </h1>
-            <p className='text-gray-600'>Sign up to use our services</p>
+  return (
+    <div className='h-screen w-full flex items-start justify-center font-inter py-8'>
+      <ToastContainer />
+      <div className='w-2/3 gap-4'>
+        <div className='flex items-start-start flex-col mb-8'>
+          <h1 className='text-4xl justified font-roboto text-black mb-2'>
+            Welcome
+          </h1>
+          <p className='text-gray-600'>Sign up to use our services</p>
+        </div>
+        <Form
+          className='flex flex-col space-y-2 mb-3 text-secondary'
+          form={signupForm}
+          name='horizontal_login'
+          layout='inline'
+          onFinish={handleSubmit}>
+          <Form.Item
+            name='first_name'
+            className='w-full '
+            rules={[yupSync]}>
+            <Input
+              className='h-9'
+              placeholder='First Name'
+            />
+          </Form.Item>
+          <Form.Item
+            name='last_name'
+            className='w-full '
+            rules={[yupSync]}>
+            <Input
+              className='h-9'
+              placeholder='Last Name'
+            />
+          </Form.Item>
+          <Form.Item
+            name='email'
+            className='w-full '
+            rules={[yupSync]}>
+            <Input
+              className='h-9'
+              placeholder='Email'
+            />
+          </Form.Item>
+          <Form.Item
+            name='password'
+            className='w-full'
+            rules={[yupSync]}>
+            <Input.Password
+              type='password'
+              className='h-9'
+              placeholder='Password'
+            />
+          </Form.Item>
+          <Form.Item
+            name='organization'
+            className='w-full '
+            rules={[yupSync]}>
+            <Input
+              className='h-9'
+              placeholder='Organization'
+            />
+          </Form.Item>
+          <Form.Item className='w-full'>
+            <Button
+              disabled={isLoading}
+              className='bg-primary font-medium text-sm py-3 w-full flex items-center justify-center text-white cursor-pointer border border-[primary] hover:bg-white hover:text-primary transition duration-150 ease-linear rounded-3xl my-3'
+              htmlType='submit'>
+              {isLoading ? (
+                <div className='flex items-center justify-center gap-x-3 bg-transparent'>
+                  <div className='spinner'></div>
+                  <span>Processing . . .</span>
+                </div>
+              ) : (
+                "Create Account"
+              )}
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <div className='text-center pt-2 mt-3'>
+          <div className='text-12 flex justify-center items-center'>
+            <p className='text-black'>I have an account?</p>
+            <Link
+              href='/login'
+              className=' font-medium pl-2'>
+              <span className='text-blue-500 hover:underline'>Sign in</span>
+            </Link>
           </div>
-
-          {/* Signup Form */}
-          {isError && (
-            <div className='text-red-500'>
-              You are not connected to the internet!
-            </div>
-          )}
-          <Form
-            className='flex flex-col space-y-2 mb-3 text-secondary'
-            form={signupForm}
-            name='horizontal_login'
-            layout='inline'
-            onFinish={handleSubmit}>
-            <Form.Item
-              name='first_name'
-              className='w-full '
-              rules={[yupSync]}>
-              <Input
-                className='h-9'
-                placeholder='First Name'
-              />
-            </Form.Item>
-            <Form.Item
-              name='last_name'
-              className='w-full '
-              rules={[yupSync]}>
-              <Input
-                className='h-9'
-                placeholder='Last Name'
-              />
-            </Form.Item>
-            <Form.Item
-              name='email'
-              className='w-full '
-              rules={[yupSync]}>
-              <Input
-                className='h-9'
-                placeholder='Email'
-              />
-            </Form.Item>
-            <Form.Item
-              name='password'
-              className='w-full'
-              rules={[yupSync]}>
-              <Input.Password
-                type='password'
-                className='h-9'
-                placeholder='Password'
-              />
-            </Form.Item>
-            <Form.Item
-              name='organization'
-              className='w-full '
-              rules={[yupSync]}>
-              <Input
-                className='h-9'
-                placeholder='Organization'
-              />
-            </Form.Item>
-            <Form.Item className='w-full'>
-              <Button
-                disabled={isLoading}
-                className='bg-primary font-medium text-sm py-3 w-full flex items-center justify-center text-white cursor-pointer border border-[primary] hover:bg-white hover:text-primary transition duration-150 ease-linear rounded-3xl my-3'
-                htmlType='submit'>
-                {isLoading ? (
-                  <div className='flex items-center justify-center gap-x-3 bg-transparent'>
-                    <div className='spinner'></div>
-                    <span>Processing . . .</span>
-                  </div>
-                ) : (
-                  "Create Account"
-                )}
-              </Button>
-            </Form.Item>
-          </Form>
-          {/* End of Signup Form */}
-
-          {/* Terms and Conditions */}
-          {/* <div className='text-center pt-2 mt-3'>
-          <div className='text-11 flex justify-center items-center'>
-            <p className='text-black'>
-              By continuing you confirmed that you agreed to our{" "}
-              <span className='text-blue-500 hover:underline'>
-                Terms and conditions
-              </span>{" "}
-              &{" "}
-              <span className='text-blue-500 hover:underline'>
-                Privacy policy.
-              </span>{" "}
-            </p>
-          </div>
-        </div> */}
-          {/* End of Terms and Conditions */}
-
-          {/* Signin Link */}
-          <div className='text-center pt-2 mt-3'>
-            <div className='text-12 flex justify-center items-center'>
-              <p className='text-black'>I have an account?</p>
-              <Link
-                href='/login'
-                className=' font-medium pl-2'>
-                <span className='text-blue-500 hover:underline'>Sign in</span>
-              </Link>
-            </div>
-          </div>
-          {/* End of Signin Link */}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 }
