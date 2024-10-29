427ecb1b3da21e1e6c28184bfee71074a50c7143
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-10-29 23:57:00 +0300

    .

--- app/medical-records/PatientCards.tsx
+++ app/medical-records/PatientCards.tsx
@@ -98,7 +98,7 @@ const PatientCards: React.FC = () => {
               required
             />
             <button
-              type='submit'
+              type='button'
               className='text-white absolute end-2.5 bottom-2.5 bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 '>
               Search
             </button>
